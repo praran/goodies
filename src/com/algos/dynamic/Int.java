6f23ee2486485ca38cc3be6e623d4dfd2a610f90
praran <pradeep.muralidharan@gmail.com>
2015-09-21 16:13:14 +0100

    actual

--- src/com/algos/dynamic/Int.java
+++ src/com/algos/dynamic/Int.java
@@ -10,8 +10,48 @@ public class Int {
 
     public static void main(String[] args){
 
-         formatParenthesis("(()");
+         formatP(")(a(b))((c)");
     }
+
+
+    public static void formatP(String str){
+
+        if(str ==null || str.length() ==0) return;
+
+        Stack<Integer> stack = new Stack<Integer>();
+
+        StringBuffer result = new StringBuffer();
+
+
+        for(int i=0; i< str.length(); i++){
+            char c = str.charAt(i);
+            if(c == '('){
+                stack.add(i);
+            }else if( c == ')'){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }else{
+                    stack.add(i);
+                }
+
+            }
+
+        }
+
+
+        for(int i = str.length()-1; i>=0; --i){
+            int tmp = stack.peek();
+            if(tmp == i){
+                stack.pop();
+            }else{
+                result.append(str.charAt(i));
+            }
+
+        }
+
+        System.out.println(result.reverse().toString());
+    }
+
 
     public static void formatParenthesis(String str){
 
@@ -27,7 +67,12 @@ public class Int {
             if(c == '('){
                 stack.push(i);
             }else if( c == ')'){
-                stack.pop();
+                if(!stack.empty()){
+                    stack.pop();
+                }else{
+                    stack.push(i);
+                }
+
             }
 
         }
